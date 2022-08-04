4d179f1b68f47f72c93d7e4b238524040b238e01
ali shahsavand inanloo <alishahsavandinanloo@gmail.com>
2022-08-04 12:12:08 +0430

    Merged state pattern exercise.

--- State/Exercise/Driving.cs
+++ State/Exercise/Driving.cs
@@ -2,11 +2,8 @@
 
 namespace DesignPatterns.State.Exercise
 {
-<<<<<<< HEAD
     public class Driving : ITravelMode // ConcreteState 
-=======
-    public class Driving : ITravelMode
->>>>>>> f332b3aa3a1bfab77b451b4fa85c852b05622048
+
     {
         public object GetEta()
         {
--- State/Exercise/Transit.cs
+++ State/Exercise/Transit.cs
@@ -2,11 +2,8 @@
 
 namespace DesignPatterns.State.Exercise
 {
-<<<<<<< HEAD
     public class Transit : ITravelMode // ConcreteState 
-=======
-    public class Transit : ITravelMode
->>>>>>> f332b3aa3a1bfab77b451b4fa85c852b05622048
+
     {
         public object GetEta()
         {
